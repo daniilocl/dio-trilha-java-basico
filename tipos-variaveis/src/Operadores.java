5477d0a3d9b74168d5b4a56f8b1aa8c5bad626bd
@daniilocl <danilocarvalho.matos67@gmail.com>
2025-04-15 15:24:29 -0300

    meu projeto conta banco

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+
+        if(numero1 < numero2);{
+            System.out.println("A nossa condição é verdadeira");
+        }
+/* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+if(a==b)
+   resultado = "verdadeiro";
+else
+   resultado = "falso";
+*/
+    }
+}
